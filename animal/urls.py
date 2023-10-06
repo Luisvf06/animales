dd92999f17ac953b9ba606e957710e6f88a60337
Luis Vazquez Franco <luis.vazquez.franco.al@iespoligonosur.org>
2023-10-06 12:46:03 +0200

    falta que coloraboradores devuelva los datos y protectora no funciona

---
+++ animal/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('animal/',views.animal_list,name='animal_list'),
+    path('colaborador/',views.colaborador_list,name='colaborador_list'),
+    path('protectora/',views.protectora_list,name='protectora_list')
+]
--- animal/views.py
+++ animal/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Animal
+from .models import Colaborador
+from .models import Protectora
 # Create your views here.
+def animal_list(request):
+    animales = Animal.objects.all()
+    return render(request,'animal/animal.html',{'animal_mostrar':animales})
+
+def colaborador_list(request):
+    colaboradores = Colaborador.objects.all()
+    return render(request,'animal/colaborador.html',{'colaborador_mostrar':colaboradores})
+
+def protectora_list(request):
+    protectoras = Protectora.objects.all()
+    return render(request,'<animal/protectora.html',{'protectora_mostrar':protectoras})
